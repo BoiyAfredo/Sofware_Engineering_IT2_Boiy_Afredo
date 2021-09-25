1fd13153a2969494603fa400870c490101243653
BoiyAfredo <58380312+BoiyAfredo@users.noreply.github.com>
2021-09-25 14:10:30 +0700

    Create w1asdos.py

---
+++ w1asdos.py
@@ -0,0 +1,9 @@
+a = b
+Nama_orang = ["Boiy", "Afredo", "Kambing"]
+Nama_orang[-2:]
+
+Nama_orang.append(22)
+print(Nama_orang)
+
+a = ["Bambang1", "Bambang2", "Bambang3", "Bambang4"]
+printf(a[-2:-1]
